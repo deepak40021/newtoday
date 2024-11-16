35d2e5ac44f25cc9c488fe2a53d0ad70abf32080
deepak40021 <deepakforwork10@gmail.com>
2024-11-16 21:16:16 +0530

    .

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ let curSelectedNav = null;
 function onNavItemClick(id) {
     fetchNews(id);
     const navItem = document.getElementById(id);
-    curSelectedNav?.classList.remove("active");
+    curSelectedNav.classList.remove("active");
     curSelectedNav = navItem;
     curSelectedNav.classList.add("active");
 }
